2dccfef5a4521a2436eefeb6a04311a29d014ba9
harshit031196 <35399002+harshit031196@users.noreply.github.com>
2020-10-13 00:57:58 +0530

    Added the search function
    Added the search function which searches for the given key and returns true and false if present or not respectively

--- BinaryTree.java
+++ BinaryTree.java
@@ -26,5 +26,19 @@ public class BinaryTree<K extends Comparable<K>> {
 	private int getSizeRecursive(BinaryNode<K> current) {
 		return current==null?0:1+this.getSizeRecursive(current.left)+this.getSizeRecursive(current.right);
 	}
+	public boolean searchKey(K key) {
+		return search(this.root,key);
+	}
+	private boolean search(BinaryNode<K> current, K key) {
+		if(current==null)return false;
+		int compareResult = key.compareTo(current.key);
+		if(compareResult==0) {
+			return true;
+		}else if(compareResult<0) {
+			return search(current.left,key);
+		}else {
+			return search(current.right,key);
+		}
+	}
 	
 }
